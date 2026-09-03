a0116f3ce916d60452b584b54ad37cea30da02af
Hipposgrumm <hipposgrumm@gmail.com>
2026-09-02 18:43:14 -0600

    Snatch-It Seemingly Working

---
+++ src/CommandHandlers/SnatchItHandlers.cs
@@ -0,0 +1,198 @@
+﻿using System.Timers;
+using sodoffmmo.Attributes;
+using sodoffmmo.Core;
+using sodoffmmo.Data;
+
+namespace sodoffmmo.CommandHandlers;
+
+// Join Any Room
+[ExtensionCommandHandler("si.JAR")]
+class SnatchItJoinRoomHandler : CommandHandler {
+    public override Task Handle(Client client, NetworkObject receivedObject) {
+        NetworkObject p = receivedObject.Get<NetworkObject>("p");
+        SnatchItRoom.Join(client, p.Get<string>("RG"));
+        return Task.CompletedTask;
+    }
+}
+
+// Lobby User Ready
+[ExtensionCommandHandler("si.LUR")]
+class SnatchItLobbyUserReadyHandler : CommandHandler {
+    public override Task Handle(Client client, NetworkObject receivedObject) {
+        SnatchItRoom room = (client.Room as SnatchItRoom)!;
+        room.SetPlayerReady(client);
+
+        NetworkPacket packet = Utils.ArrNetworkPacket([
+            "LUR",
+            room.Id.ToString(),
+            client.PlayerData.Uid
+        ], "msg", room.Id);
+
+        room.Send(packet);
+
+        if (room.GetReadyCount() > 1) {
+            room.OnGameStart();
+            packet = Utils.ArrNetworkPacket([
+                "LCDD", // Lobby CountDown Done
+                room.Id.ToString(),
+                client.PlayerData.Uid
+            ], "msg", room.Id);
+            
+            room.Send(packet);
+        }
+        return Task.CompletedTask;
+    }
+}
+
+// Lobby User Not Ready
+[ExtensionCommandHandler("si.LUNR")]
+class SnatchItLobbyUserNotReadyHandler : CommandHandler  {
+    public override Task Handle(Client client, NetworkObject receivedObject) {
+        SnatchItRoom room = (client.Room as SnatchItRoom)!;
+        room.SetPlayerReady(client, false);
+
+        NetworkPacket packet = Utils.ArrNetworkPacket([
+            "LUNR",
+            room.Id.ToString(),
+            client.PlayerData.Uid
+        ], "msg", room.Id);
+
+        room.Send(packet);
+        return Task.CompletedTask;
+    }
+}
+
+// Game Level Load
+[ExtensionCommandHandler("si.GLL")]
+class SnatchItLevelLoadHandler : CommandHandler {
+    public override Task Handle(Client client, NetworkObject receivedObject) {
+        SnatchItRoom room = (client.Room as SnatchItRoom)!;
+        NetworkObject p = receivedObject.Get<NetworkObject>("p");
+        
+        NetworkPacket packet = Utils.ArrNetworkPacket([
+            "GLL", // Game CountDown Start
+            room.Id.ToString(),
+            p.Get<string>("0")
+        ], "msg", room.Id);
+        room.Send(packet);
+        return Task.CompletedTask;
+    }
+}
+
+// Game Level Loaded
+[ExtensionCommandHandler("si.GLLD")]
+class SnatchItLevelLoadedHandler : CommandHandler  {
+    private System.Timers.Timer? timer = null;
+    private int counter;
+    private SnatchItRoom room;
+
+    public override Task Handle(Client client, NetworkObject receivedObject) {
+        room = (client.Room as SnatchItRoom)!;
+        counter = 5;
+        
+        NetworkPacket packet = Utils.ArrNetworkPacket([
+            "GCDS", // Game CountDown Start
+            room.Id.ToString(),
+            (--counter).ToString()
+        ], "msg", room.Id);
+        room.Send(packet);
+        
+        timer = new System.Timers.Timer(1500);
+        timer.AutoReset = true;
+        timer.Enabled = true;
+        timer.Elapsed += OnTick;
+        return Task.CompletedTask;
+    }
+
+    private void OnTick(Object? source, ElapsedEventArgs e) {
+        NetworkPacket packet;
+        if (--counter > 0) {
+            packet = Utils.ArrNetworkPacket([
+                "GCDU", // Game CountDown Update
+                room.Id.ToString(),
+                counter.ToString()
+            ], "msg", room.Id);
+        } else {
+            packet = Utils.ArrNetworkPacket([
+                "GS", // Game Start
+                room.Id.ToString()
+            ], "msg", room.Id);
+            
+            timer!.Stop();
+            timer!.Close();
+            timer = null;
+        }
+        room.Send(packet);
+    }
+}
+
+// Relay Game Data
+[ExtensionCommandHandler("si.RGD")]
+class SnatchItRelayGameDataHandler : CommandHandler {
+    public override Task Handle(Client client, NetworkObject receivedObject) {
+        SnatchItRoom room = (client.Room as SnatchItRoom)!;
+        NetworkObject p = receivedObject.Get<NetworkObject>("p");
+        
+        NetworkPacket packet = Utils.ArrNetworkPacket([
+            "RGD", // Relay Game Data
+            room.Id.ToString(),
+            p.Get<string>("0"),
+            p.Get<string>("1"),
+            p.Get<string>("2")
+        ], "msg", room.Id);
+        room.Send(packet, client);
+        return Task.CompletedTask;
+    }
+}
+
+// Game Complete
+[ExtensionCommandHandler("si.GC")]
+class SnatchItGameCompleteHandler : CommandHandler  {
+    public override Task Handle(Client client, NetworkObject receivedObject) {
+        SnatchItRoom room = (client.Room as SnatchItRoom)!;
+        NetworkObject p = receivedObject.Get<NetworkObject>("p");
+        
+        room.ProcessResult(client, p.Get<string>("0"), p.Get<string>("1"));
+        return Task.CompletedTask;
+    }
+}
+
+// Play Again
+[ExtensionCommandHandler("si.PA")]
+class SnatchItPlayAgainHandler : CommandHandler {
+    public override Task Handle(Client client, NetworkObject receivedObject) {
+        GauntletRoom room = (client.Room as GauntletRoom)!;
+        room.SetPlayerReady(client, false);
+
+        NetworkPacket packet = Utils.ArrNetworkPacket([
+            "LUNR",
+            room.Id.ToString(),
+            client.PlayerData.Uid
+        ], "msg", room.Id);
+
+        room.Send(packet, client);
+        room.SendPA(client);
+        return Task.CompletedTask;
+    }
+}
+
+// SnatchIt Pet Name
+[ExtensionCommandHandler("si.SNPN")]
+class SnatchItPetNameHandler : CommandHandler  {
+    public override Task Handle(Client client, NetworkObject receivedObject) {
+        SnatchItRoom room = (client.Room as SnatchItRoom)!;
+        NetworkObject p = receivedObject.Get<NetworkObject>("p");
+        string petname = p.Get<string>("0");
+        room.AssignPetName(client, petname);
+
+        NetworkPacket packet = Utils.ArrNetworkPacket([
+            "SNPN",
+            room.Id.ToString(),
+            client.PlayerData.Uid,
+            petname
+        ], "msg", room.Id);
+        
+        room.Send(packet);
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Core/Rooms/SnatchItRoom.cs
@@ -0,0 +1,82 @@
+using System;
+using sodoffmmo.Data;
+
+namespace sodoffmmo.Core;
+
+public class SnatchItRoom : HeadToHeadRoom {
+    private class SnatchItMatchmakingHandler : MatchmakingHandler {
+        internal static readonly SnatchItMatchmakingHandler instance = new();
+        protected override int _MaxPlayers => 2;
+        protected override HeadToHeadRoom CreateNewInstance(string roomgroup) {
+            return new SnatchItRoom(roomgroup);
+        }
+    }
+
+    protected class SnatchItStatus : Status {
+        public string petname;
+
+        public string? resultScore = null;
+        public string? resultCollectCount = null;
+
+        public SnatchItStatus(string uid) : base(uid) {}
+    }
+
+    public SnatchItRoom(string roomname) : base(roomname) {}
+    
+    public override void AddPlayer(Client client) {
+        players[client] = new SnatchItStatus(client.PlayerData.Uid);
+    }
+
+    protected override string[] WritePlayer(KeyValuePair<Client, Status> player) => [
+        player.Value.uid,
+        player.Value.isReady.ToString(),
+        (player.Value as SnatchItStatus)!.petname+'/'
+                                                 +"0" // ImageSlotIndex - Unknown and it's not used anywhere.
+    ];
+
+    protected override string[] AddDataJoin() => [
+        base.Id.ToString()
+    ];
+    
+    protected override string[] AddDataLeave() => [
+        base.Id.ToString()
+    ];
+    
+    protected override string[] AddDataPlayAgain() => [
+        base.Id.ToString()
+    ];
+
+    public void AssignPetName(Client client, string petname) {
+        (players[client] as SnatchItStatus)!.petname = petname;
+    }
+    
+    public bool ProcessResult(Client client, string score, string collectCount) {
+        lock (base.roomLock) {
+            var clientStatus = (players[client] as SnatchItStatus)!;
+            clientStatus.resultScore = score;
+            clientStatus.resultCollectCount = collectCount;
+
+            if (players.Any(p => (p.Value as SnatchItStatus)!.resultScore == null))
+                return false;
+            
+            List<string> info = new();
+            info.Add("GC");
+            info.Add(base.Id.ToString());
+            foreach(var player in players) {
+                var status = (player.Value as SnatchItStatus)!;
+                if (status.resultScore == null) continue;
+                info.Add(status.uid);
+                info.Add(status.resultScore!);
+                info.Add(status.resultCollectCount!);
+            }
+            NetworkPacket packet = Utils.ArrNetworkPacket(info.ToArray(), "msg", base.Id);
+
+            Send(packet);
+            return true;
+        }
+    }
+
+    static public void Join(Client client, string roomgroup) {
+        SnatchItMatchmakingHandler.instance.Join(client, roomgroup);
+    }
+}
